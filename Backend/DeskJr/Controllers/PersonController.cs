7ca9828ef78ef042b9817d3170941613ccd0387a
BerfinSavran <berfinsavran@gmail.com>
2024-07-04 11:21:09 +0300

    PersonController eklendi

---
+++ Backend/DeskJr/Controllers/PersonController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeskJr.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+    }
+}
