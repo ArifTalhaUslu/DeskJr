5f4182c4e253b22648ca377e68c16748959565f9
BerfinSavran <berfinsavran@gmail.com>
2024-07-09 17:22:14 +0300

    Employee Repository eklendi.

---
+++ DeskJr.Repository/Abstract/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using DeskJr.Entity.Models;
+
+namespace DeskJr.Repository.Abstract;
+
+public interface IEmployeeRepository
+{
+    public Task<bool> AddAsync(Employee employee);
+    public Task<bool> UpdateAsync(Employee employee);
+    public Task<bool> DeleteAsync(Guid id);
+    public Task<List<Employee>> GetAllAsync();
+    public Task<Employee> GetByIdAsync(Guid id);
+
+}
