de053643cb9007007cd54e842854bcac76eecdbb
BerfinSavran <berfinsavran@gmail.com>
2024-07-29 11:25:33 +0300

    fix

--- DeskJr.Data/Migrations/AppDbContextModelSnapshot.cs
+++ DeskJr.Data/Migrations/AppDbContextModelSnapshot.cs
@@ -62,7 +62,7 @@ namespace DeskJr.Data.Migrations
 
                     b.HasIndex("TitleId");
 
-                    b.ToTable("Employees");
+                    b.ToTable("Employees", (string)null);
                 });
 
             modelBuilder.Entity("DeskJr.Entity.Models.EmployeeTitle", b =>
@@ -80,7 +80,7 @@ namespace DeskJr.Data.Migrations
                     b.HasIndex("TitleName")
                         .IsUnique();
 
-                    b.ToTable("EmployeeTitles");
+                    b.ToTable("EmployeeTitles", (string)null);
                 });
 
             modelBuilder.Entity("DeskJr.Entity.Models.Leave", b =>
@@ -125,7 +125,7 @@ namespace DeskJr.Data.Migrations
 
                     b.HasIndex("RequestingEmployeeId");
 
-                    b.ToTable("Leaves");
+                    b.ToTable("Leaves", (string)null);
                 });
 
             modelBuilder.Entity("DeskJr.Entity.Models.LeaveType", b =>
@@ -146,7 +146,7 @@ namespace DeskJr.Data.Migrations
 
                     b.HasKey("ID");
 
-                    b.ToTable("LeaveTypes");
+                    b.ToTable("LeaveTypes", (string)null);
                 });
 
             modelBuilder.Entity("DeskJr.Entity.Models.Team", b =>
@@ -164,7 +164,7 @@ namespace DeskJr.Data.Migrations
 
                     b.HasKey("ID");
 
-                    b.ToTable("Teams");
+                    b.ToTable("Teams", (string)null);
                 });
 
             modelBuilder.Entity("DeskJr.Entity.Models.Employee", b =>
