42cc291e724f1a56ba9060c12594c85ba257dbf6
Ali Aksu <aliaksu00@hotmail.com>
2024-08-01 17:26:10 +0300

    create seed data

--- DeskJr.Data/AppDbContext.cs
+++ DeskJr.Data/AppDbContext.cs
@@ -20,30 +20,71 @@ namespace DeskJr.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Employee>().Property(d => d.Name).HasColumnType("VARCHAR").HasMaxLength(150).IsRequired();
-            modelBuilder.Entity<EmployeeTitle>()
-                                      .HasIndex(t => t.TitleName)
-                                      .IsUnique();
+            #region Data Seed
+
+            var teamId = Guid.NewGuid();
+            var employeeId = Guid.NewGuid();
+            var titleId = Guid.NewGuid();
 
             modelBuilder.Entity<Team>()
-                   .HasOne(t => t.Manager)
-                   .WithMany()
-                   .HasForeignKey(t => t.ManagerId)
-                   .OnDelete(DeleteBehavior.Restrict); // Silme davranışını belirlemek önemli
+                .HasData(
+                    new Team()
+                    {
+                        Name = "Default Team",
+                        ID = teamId,
+                    }
+                );
+
+            modelBuilder.Entity<EmployeeTitle>()
+                .HasData(
+                    new EmployeeTitle()
+                    {
+                        TitleName = "Default Title",
+                        ID = titleId,
+                    }
+                );
 
             modelBuilder.Entity<Employee>()
-                    .HasOne(e => e.Team)
-                    .WithMany() // Eğer Team ile birden fazla Employee ilişkisi varsa WithMany
-                    .HasForeignKey(e => e.TeamId)
-                    .OnDelete(DeleteBehavior.Restrict); // Silme davranışını belirlemek önemli
+                .HasData(
+                    new Employee()
+                    {
+                        ID = employeeId,
+                        Name = "Admin",
+                        DayOfBirth = DateTime.Now.AddYears(-23),
+                        Gender = Entity.Types.EnumGender.Male,
+                        EmployeeRole = Entity.Types.EnumRole.Administrator,
+                        Email = "admin@deskjr.com",
+                        Password = "202CB962AC59075B964B07152D234B70",
+                        TeamId = teamId,
+                        EmployeeTitleId = titleId,
+                    }
+                );
+            #endregion
+
+            modelBuilder.Entity<Employee>().Property(d => d.Name).HasColumnType("VARCHAR").HasMaxLength(150).IsRequired();
+                modelBuilder.Entity<EmployeeTitle>()
+                                          .HasIndex(t => t.TitleName)
+                                          .IsUnique();
+
+                modelBuilder.Entity<Team>()
+                       .HasOne(t => t.Manager)
+                       .WithMany()
+                       .HasForeignKey(t => t.ManagerId)
+                       .OnDelete(DeleteBehavior.Restrict); // Silme davranışını belirlemek önemli
+
+                modelBuilder.Entity<Employee>()
+                        .HasOne(e => e.Team)
+                        .WithMany() // Eğer Team ile birden fazla Employee ilişkisi varsa WithMany
+                        .HasForeignKey(e => e.TeamId)
+                        .OnDelete(DeleteBehavior.Restrict); // Silme davranışını belirlemek önemli
 
 
         }
-        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=test1;User Id=SA;Password=Ncc-1701;Integrated Security=False;Encrypt=False");
-            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=test1;User Id=SA;Password=Ncc-1701;Integrated Security=False;Encrypt=False");
+                optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 
-        }*/
+            }*/
     }
 }
---
+++ DeskJr.Data/Migrations/20240801141640_v4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeskJr.Data.Migrations
+{
+    public partial class v4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "EmployeeTitles",
+                columns: new[] { "ID", "TitleName" },
+                values: new object[] { new Guid("2bee94c8-56fe-4b90-8e0b-4789b6dca2df"), "Default Title" });
+
+            migrationBuilder.InsertData(
+                table: "Teams",
+                columns: new[] { "ID", "ManagerId", "Name" },
+                values: new object[] { new Guid("98ec940f-0387-4bf7-b847-78d23184729a"), null, "Default Team" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "ID", "DayOfBirth", "Email", "EmployeeRole", "EmployeeTitleId", "Gender", "Name", "Password", "TeamId" },
+                values: new object[] { new Guid("1a3627b3-208e-49b1-8b8c-334d805a3a65"), new DateTime(2001, 8, 1, 17, 16, 39, 802, DateTimeKind.Local).AddTicks(4770), "admin@deskjr.com", 0, new Guid("2bee94c8-56fe-4b90-8e0b-4789b6dca2df"), 0, "Admin", "202CB962AC59075B964B07152D234B70", new Guid("98ec940f-0387-4bf7-b847-78d23184729a") });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "ID",
+                keyValue: new Guid("1a3627b3-208e-49b1-8b8c-334d805a3a65"));
+
+            migrationBuilder.DeleteData(
+                table: "EmployeeTitles",
+                keyColumn: "ID",
+                keyValue: new Guid("2bee94c8-56fe-4b90-8e0b-4789b6dca2df"));
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "ID",
+                keyValue: new Guid("98ec940f-0387-4bf7-b847-78d23184729a"));
+        }
+    }
+}
